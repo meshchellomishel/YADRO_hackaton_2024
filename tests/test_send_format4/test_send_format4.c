b46c923173a488abc4ee2beecaad174fa46cce85
Sukhanov Mikhail <thetrenshow157@gmail.com>
2024-04-20 14:39:21 +0000

    Mikhail: tests almost done, check test_iir_all

--- tests/test_send_format4/test_send_format4.c
+++ tests/test_send_format4/test_send_format4.c
@@ -19,52 +19,43 @@
 #define UART_REG_SCR ( UART_BASE_ADDR + 0x07) // Scratch Register
 
 
-#define UART_IRR_SUCCESS    0xC0
+#define UART_LSR_THR_EMPTY_5    0b00100000
+#define UART_LSR_THR_EMPTY_6    0b01000000
+#define UART_LSR_PARITY_ERR     0b00000100
+#define UART_LSR_DATA_AVAILABLE 0b00000001
 
+#define UART_LSR_ERR_MASK       0b01100101
 
+#define FMT_MASK    0b11111111
 
-static void init_apb()
+static void init_apb(uint8_t data_size)
 {
     int key;
 
     key = UART_REG_LCR;
-    WRITE_MEMORY(key, 8, 3 | 0x80);
+    WRITE_MEMORY(key, 8, data_size | 0x80);
 
     key = UART_REG_DLL;
     WRITE_MEMORY(key, 8, 100);
 
     key = UART_REG_LCR;
-    WRITE_MEMORY(key, 8, 3);
+    WRITE_MEMORY(key, 8, data_size);
 }
-
-
-#define UART_LSR_THR_EMPTY      0b01000000
-#define UART_LSR_PARITY_ERR     0b01000100
-#define UART_LSR_DATA_AVAILABLE 0b00000001
 
 int main(void)
 {
-    sc_printf("Test: XMIT TEST\n");
+    sc_printf("Test: FORMAT(8 bits) TEST\n");
 
     int key;
     int ret_val = 0;
-    uint8_t read, write = 15;
+    uint8_t read, write = 0b11111111;
 
-    key = UART_REG_IER;
-    WRITE_MEMORY(key, 8, 7);
-
-    init_apb();
-
-    key = UART_REG_LSR;
-    read = READ_MEMORY(key, 8);
-    if (!(read & UART_LSR_THR_EMPTY)) {
-        sc_printf("\t[ERROR]: test lsr: tx fofo not empty\n");
-        ret_val = 1;
-        goto on_exit;
-    }
+    init_apb(3);
 
     key = UART_REG_THR;
     WRITE_MEMORY(key, 8, write);
+
+    write &= FMT_MASK;
 
     read = 0;
     while (!(read & UART_LSR_DATA_AVAILABLE) || (read & UART_LSR_PARITY_ERR))
@@ -72,11 +63,11 @@ int main(void)
 
     key = UART_REG_RBR;
     read = READ_MEMORY(key, 8);
-
     if (read != write) {
-        sc_printf("\n[ERROR]: rx data: %d, tx data %d\n", read, write);
+        sc_printf("\t[FMT8BITS]: rx data: %d, tx data %d\n", read, write);
         ret_val = 1;
     }
+
 
 
 on_exit:
